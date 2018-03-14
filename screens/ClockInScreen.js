685c11cf91bb33a7391a3512e61efba555a78631
Riley MacDonald <riley@Rileys-MacBook-Air.local>
2018-03-14 18:02:41 -0400

    Added screens

---
+++ screens/ClockInScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ExpoConfigView } from '@expo/samples';
+
+export default class ClockInScreen extends React.Component {
+  static navigationOptions = {};
+
+  render() {
+    /* Go ahead and delete ExpoConfigView and replace it with your
+     * content, we just wanted to give you a quick view of your config */
+    return <ExpoConfigView />;
+  }
+}
